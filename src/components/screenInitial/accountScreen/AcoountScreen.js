9eabcb0ee596e44a2cd4415692bc1c51558f86c0
alexis <alexischavez1234.ac@gmail.com>
2021-10-13 00:56:27 -0500

    delete user

--- src/components/screenInitial/accountScreen/AcoountScreen.js
+++ src/components/screenInitial/accountScreen/AcoountScreen.js
@@ -4,14 +4,17 @@ import { useSelector } from 'react-redux';
 import { deleteAccoout, updateDisplayNameAsync } from '../../../actions/auth';
 import { deleteAllTodoAsync } from '../../../actions/todo';
 import { useForm } from '../../../hooks/useForm';
+import close_button from '../../../pictures/close_black_24dp.svg';
+
+
 
 export const AcoountScreen = () => {
-    
-    const dispatch = useDispatch();
-    const {photoURL, name} = useSelector(state => state.auth);
-    const [disabled, setdisabled] = useState(true);
 
-    const [ {name: nombre}, handleInputChange, setValues ] = useForm({
+    const dispatch = useDispatch();
+    const { photoURL, name } = useSelector(state => state.auth);
+    const [ disabled, setdisabled ] = useState(true);
+    const [ modal, setmodal ] = useState(false);
+    const [ { name: nombre }, handleInputChange, setValues ] = useForm({
         name,
     });
     const nameref = useRef(nombre);
@@ -23,9 +26,9 @@ export const AcoountScreen = () => {
     }
     const handleUpdate = (e) => {
         e.preventDefault();
-        if( nombre === nameref.current ){
+        if (nombre === nameref.current) {
             setdisabled(true);
-        }else{
+        } else {
             setdisabled(true);
             dispatch(updateDisplayNameAsync(nombre));
             nameref.current = nombre;
@@ -38,64 +41,90 @@ export const AcoountScreen = () => {
         setdisabled(true);
     }
     const handleDeleteAccount = () => {
-        // dispatch(deleteAccoout());
-        // dispatch(deleteAllTodoAsync());
+        dispatch(deleteAccoout());
+        dispatch(deleteAllTodoAsync());
+    }
+    const handleShowModal = () => {
+        setmodal(!modal);
     }
 
     return (
         <div className="container-account">
             <section>
                 <article>
-                <img src={photoURL} alt="photoURL" />
-                <span>{nombre}</span>
+                    <img src={photoURL} alt="photoURL" />
+                    <span>{nombre}</span>
                 </article>
                 <article>
                     <form>
                         <label>
-                            Name: 
-                        <input 
-                        type="text" 
-                        disabled={disabled} 
-                        className={disabled? 'disabled': 'input'}
-                        name="name"
-                        value={nombre}
-                        onChange={handleInputChange}
-                        ref={ref}
-                        />
-                        {
-                            disabled ?(
+                            Name:
+                            <input
+                                type="text"
+                                disabled={disabled}
+                                className={disabled ? 'disabled' : 'input'}
+                                name="name"
+                                value={nombre}
+                                onChange={handleInputChange}
+                                ref={ref}
+                            />
+                            {
+                                disabled ? (
 
-                                <button
-                                type="button" 
-                                className="btn btn-primary"
-                                onClick={handleChangeStatus}
-                                >Change</button>
-                            ) : (
-                                <>
-                                <button
-                                type="submit"
-                                className="btn btn-secoundary"
-                                onClick={handleUpdate}
-                                >Update</button>
+                                    <button
+                                        type="button"
+                                        className="btn btn-primary"
+                                        onClick={handleChangeStatus}
+                                    >Change</button>
+                                ) : (
+                                    <>
+                                        <button
+                                            type="submit"
+                                            className="btn btn-secoundary"
+                                            onClick={handleUpdate}
+                                        >Update</button>
 
-                                <button
-                                type="button" 
-                                className="btn btn-danger"
-                                onClick={ handleCancel }
-                                >Cancel</button>
-                                </>
-                            )
-                        }
+                                        <button
+                                            type="button"
+                                            className="btn btn-danger"
+                                            onClick={handleCancel}
+                                        >Cancel</button>
+                                    </>
+                                )
+                            }
                         </label>
                     </form>
                 </article>
                 <span>CAUTION: DANGER ZONE</span>
                 <article className="danger-zone">
-                   <button onClick={handleDeleteAccount} >
+                    <button onClick={handleShowModal} >
                         Delete this account
-                   </button>
+                    </button>
                 </article>
             </section>
+            {
+                modal &&
+                <div className="background">
+                    <div className="delete-account-modal">
+                        <img
+                            src={close_button}
+                            alt="close-button"
+                            onClick={handleShowModal}
+                        />
+                        <div className="alert danger">
+                            Are you sure ?
+                        </div>
+                        <div className="alert info">
+                            If you click on the button below, your account will be deleted along with all the data.
+                        </div>
+                        <button
+                        onClick={ handleDeleteAccount }
+                        >
+                            Yes i'm sure !
+                        </button>
+                    </div>
+                </div>
+            }
         </div>
     )
 }
