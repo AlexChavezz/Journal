05ea837b1b414800526ae28cf7165e33fef5c338
AlexChavezz <alexischavez1234.ac@gmail.com>
2021-12-21 12:46:33 -0600

    search layout

--- src/components/screenInitial/UI/Header.js
+++ src/components/screenInitial/UI/Header.js
@@ -1,18 +1,34 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import manageAccoutLogo from '../../../pictures/manage_accounts_white_24dp.svg';
 import { ModalOptions } from '../ModalOptions';
 import searchLogo from '../../../pictures/search_black_24dp.svg';
+import { useForm } from '../../../hooks/useForm';
+import { SearchResults } from '../search/SearchResults';
+import close from '../../../pictures/close_black_24dp.svg';
 
 export const Header = () => {
 
     const { name } = useSelector(state => state.auth);
     const [optionsModal, setoptionsModal] = useState(false);
+    const [isSearching, setIsSearching] = useState(false);
 
     const handleChangeStatus = () => {
         setoptionsModal(!optionsModal);
     }
+    const [{ search }, handleInputChange, ,reset] = useForm({
+        search: '', 
+    });
 
+    useEffect(() => {
+
+        if (search.trim().length > 0) {
+            setIsSearching(true);
+        } else {
+            setIsSearching(false);
+        }
+
+    }, [search])
 
     return (
         <header className="journal-header">
@@ -23,9 +39,31 @@ export const Header = () => {
                     <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
                 </svg>
             </div> */}
+            {/* Component to search anything */}
+            {
+                isSearching &&
+                <SearchResults search={search} />
+            }
+
             <div className="search">
                 <img src={searchLogo} alt="lupa" />
-                <input type="text" placeholder="Type to search" />
+                <input
+                    type="text"
+                    placeholder="Type to search"
+                    name="search"
+                    value={search}
+                    onChange={handleInputChange}
+                    autoComplete="off"
+                />
+                {
+                    search.trim().length > 0 &&
+                    <img
+                        src={close}
+                        alt="close"
+                        className="input-clear"
+                        onClick={()=> reset()}
+                    />
+                }
             </div>
             <div className="user dropdown">
                 <span
---
+++ src/components/screenInitial/search/SearchResults.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const SearchResults = () => {
+    return (
+        <section className="search-results">
+            
+        </section>
+    )
+}
